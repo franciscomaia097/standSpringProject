2f8941f5901b269b8de8ae94c1b32696f18d9db9
franciscomaia097 <105322731+franciscomaia097@users.noreply.github.com>
2024-02-15 12:29:27 +0000

    Add car list to stand

--- src/main/java/com/example/stand/Models/Stand.java
+++ src/main/java/com/example/stand/Models/Stand.java
@@ -1,7 +1,11 @@
 package com.example.stand.Models;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
 
 @Entity
 public class Stand {
@@ -11,11 +15,22 @@ public class Stand {
 
     private String email;
 
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Car> cars;
+
 
     public Stand(String name, String email) {
         this.id = 0;
         this.name = name;
         this.email = email;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
     }
 
     public long getId() {
