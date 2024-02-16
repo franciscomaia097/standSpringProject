45234f0943fceabe945f2c2706dfda81e7f9354c
franciscomaia097 <105322731+franciscomaia097@users.noreply.github.com>
2024-02-16 09:51:44 +0000

    Add Seller POST and GET

--- src/main/java/com/example/stand/Controllers/SellerController.java
+++ src/main/java/com/example/stand/Controllers/SellerController.java
@@ -1,10 +1,12 @@
 package com.example.stand.Controllers;
 
 
+import com.example.stand.Models.Seller;
 import com.example.stand.Services.SellerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/seller")
@@ -15,5 +17,15 @@ public class SellerController {
     @Autowired
     public SellerController(SellerService sellerService) {
         this.sellerService = sellerService;
+    }
+
+    @PostMapping("/create")
+    public Seller createSeller(@RequestBody Seller seller) {
+        return sellerService.createSeller(seller);
+    }
+
+    @GetMapping("/all")
+    public List<Seller> getAllSellers() {
+        return sellerService.getAllSellers();
     }
 }
--- src/main/java/com/example/stand/Services/SellerService.java
+++ src/main/java/com/example/stand/Services/SellerService.java
@@ -1,8 +1,11 @@
 package com.example.stand.Services;
 
+import com.example.stand.Models.Seller;
 import com.example.stand.Repositories.SellerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SellerService {
@@ -11,5 +14,13 @@ public class SellerService {
     @Autowired
     public SellerService(SellerRepository sellerRepository) {
         this.sellerRepository = sellerRepository;
+    }
+
+    public Seller createSeller(Seller seller) {
+        return sellerRepository.save(seller);
+    }
+
+    public List<Seller> getAllSellers() {
+        return sellerRepository.findAll();
     }
 }
