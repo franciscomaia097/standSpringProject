03ccd72db6d5c9131801238137f6048999a55071
franciscomaia097 <105322731+franciscomaia097@users.noreply.github.com>
2024-02-21 09:34:58 +0000

    Add DTOs

---
+++ src/main/java/com/example/stand/DTOs/CarDTO.java
@@ -0,0 +1,101 @@
+package com.example.stand.DTOs;
+
+import com.example.stand.Enums.CarStatus;
+import com.example.stand.Models.Brand;
+import com.example.stand.Models.Model;
+import com.example.stand.Models.Seller;
+
+public class CarDTO {
+
+    private long id;
+    private Seller seller;
+    private String licensePlate;
+    private Brand brand;
+    private Model model;
+    private String color;
+    private String year;
+    private String price;
+    private String description;
+    private CarStatus status;
+
+    // Getters and setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Seller getSeller() {
+        return seller;
+    }
+
+    public void setSeller(Seller seller) {
+        this.seller = seller;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
+
+    public Model getModel() {
+        return model;
+    }
+
+    public void setModel(Model model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public CarStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(CarStatus status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/com/example/stand/DTOs/SellerDTO.java
@@ -0,0 +1,24 @@
+package com.example.stand.DTOs;
+
+public class SellerDTO {
+
+    private long id;
+    private String name;
+
+    // Getters and setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
